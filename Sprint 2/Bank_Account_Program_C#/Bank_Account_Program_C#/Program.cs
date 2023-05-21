6b281a725c21913786bb9e58f721eceb0996df12
Devin Johnson <91571848+johnsondevin@users.noreply.github.com>
2023-05-20 21:28:09 -0600

    Final Project

---
+++ Sprint 2/Bank_Account_Program_C#/Bank_Account_Program_C#/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+
+namespace bank_account
+{
+    class account
+    {
+        // Identifiers of the type account
+        private int account_id;
+        private string? account_name;
+        private double account_balance;
+
+        private static int new_id = 0;
+
+        public account()
+        {
+            // Account constructor
+            account_id = new_id++;
+        }
+
+        public int Id
+        {
+            // Account Id getter
+            get { return account_id; }
+        }
+        public string? Name
+        {
+            // Account Name getter and setter
+            get { return account_name; }
+            set { account_name = value; }
+        }
+        public double Balance
+        {
+            // Account Balance getter and setter
+            get { return account_balance; }
+            set { account_balance = value; }
+        }
+
+        public void AddBalance(double value)
+        {
+            // Add a deposit to an account
+            account_balance += value;
+        }
+
+        public void SubBalance(double value)
+        {
+            // Subtract from a balance
+            account_balance -= value;
+        }
+    }
+    class functions
+    {
+        public static void Display(List<account> accounts)
+        {
+            // Display all accounts in list
+            foreach (account account in accounts)
+            {
+                Console.WriteLine("Id: " + account.Id);
+                Console.WriteLine("Name: " + account.Name);
+                Console.WriteLine("Balance: $" + account.Balance);
+            }
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        { 
+            // List of objects
+            List<account> accounts = new List<account>();
+
+            var a = new account();
+            var p = new program();
+
+            bool user_choice = true;
+            while (user_choice) // Program Menu
+            {
+                Console.WriteLine("Menu Options: ");
+                Console.WriteLine("0: End Program");
+                Console.WriteLine("1: Add an Account");
+                Console.WriteLine("2: Display Account Information");
+                Console.WriteLine("3: Add a Deposit");
+                Console.WriteLine("4: Make a Withdrawal");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        user_choice = false;
+                        break;
+                    case 1:
+                        p.CreateAccount(accounts);
+                        break;
+                    case 2:
+                        functions.Display(accounts);
+                        break;
+                    case 3:
+                        int number1;
+                        double deposit;
+
+                        Console.WriteLine("Enter the account ID: ");
+                        number1 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter the deposit amount: $");
+                        deposit = Convert.ToDouble(Console.ReadLine());
+
+                        accounts.Find(account => account.Id == number1);
+                        //FindAccount(accounts);
+
+                        break;
+                    case 4:
+                        int number2;
+                        double withdrawal;
+
+                        Console.WriteLine("Enter the account ID: ");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter the withdrawl amount: $");
+                        withdrawal = Convert.ToDouble(Console.ReadLine());
+
+                        //FindAccount(accounts);
+
+                        break;
+                }
+
+            }
+        }
+        public void CreateAccount(List<account> accounts)
+        {
+            // Create new account type
+            string name = "";
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine()!;
+
+            double balance;
+            Console.WriteLine("Enter the account balance: $");
+            balance = Convert.ToDouble(Console.ReadLine());
+
+            accounts.Add(new account() {Name = name, Balance = balance}); // Add new account to a list
+        }
+/*        public static IEnumerable<account> FindAccount(List<account> accounts)
+        {
+            // Iterate through the object list to find an account
+            int id;
+
+            Console.WriteLine("Enter the account Id: ");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            for (int it = 0; it < accounts.Count; it++)
+            {
+                Console.WriteLine("Made it to for");
+                if (it == id)
+                {
+                    Console.WriteLine("Made it to if");
+                    yield return accounts[it];
+                }
+            }
+        }*/
+    }
+}
---
+++ Sprint 2/README.md
@@ -0,0 +1,36 @@
+# Overview
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+This is a Bank Account program. It gives a simple way to add an account, view all the accounts provided, deposit to an account, and withdraw from an account.
+
+I wrote this program to learn the programming language C#. I haven't had the chance until now to use this language, and I think that it will benefit me in the future.
+
+Here is a link to a video demonstration of my project.
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+To create the program, I used Visual Studio. I was able to learn the basics about the debugger and the compiler, enough to create and run my program with this new software.
+
+I used C#, which is a language created by Microsoft for the development of their software and applications. It has a broad range of uses, including mobile and desktop
+applications. It is a very versatile language.
+
+# Useful Websites
+
+Here are some websites that I found to be very useful while creating my project.
+
+- [Microsoft.com (C# Documentation)](https://learn.microsoft.com/en-us/dotnet/csharp/)
+- [GeeksforGeeks.org (C# Tutorials)](https://www.geeksforgeeks.org/csharp-programming-language/)
+
+# Future Work
+
+Here are some things that I want to continue in order to further learn how to use C#.
+
+- I want to go through and fix the bugs in my program. There are some bugs that I wasn't able to quite work out, so I want to get some help from some
+collegues to fix the bugs.
+- I would like to learn about creating a program using C# with a user interface, instead of the console. I think this would help me learn more about
+how to create an app.
+- I want to find a way to use this language more frequently so that I don't forget everything that I just learned for this project. If I create a project
+when I have time, I think I will have a better recollection of this language for when I need it next.
